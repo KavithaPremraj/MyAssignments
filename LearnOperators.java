56ff955b63501b5043439b31aa9992195ce98afb
KavithaPremraj <117530811+KavithaPremraj@users.noreply.github.com>
2022-11-06 10:10:42 +0530

    Add files via upload

---
+++ LearnOperators.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class LearnOperators {
+	
+
+	int a = 10;
+	int b = 20;
+	int sum = a+b;
+	int sub = a-b;
+	int multiply = a*b;
+	int div = multiply/a;
+	
+
+	public static void main(String[] args) {
+		LearnOperators lo = new LearnOperators();
+		System.out.println(lo.sum);
+		System.out.println(lo.sub);
+		System.out.println(lo.multiply);
+		System.out.println(lo.div);
+		
+
+	}
+
+}
